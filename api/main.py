a496d656a7a8e2a31696a4df308ff5d07e9c3d51
Mikaio Melo de Souza <mikaio.melo@mc2performa.com.br>
2023-02-24 18:59:52 -0300

    add error management to producer

--- api/main.py
+++ api/main.py
@@ -23,18 +23,27 @@ def read_root():
 
 @app.post("/log", status_code=status.HTTP_200_OK)
 def send_message(log: Log, response: Response):
-    
-    if log.type not in LOG_TYPES:
+    try:
+        if log.type not in LOG_TYPES:
+            return Response(
+                content='Invalid log type. Use either "info", "warning" or "error".',
+                status_code=status.HTTP_400_BAD_REQUEST
+            )
+        
+        data = {
+            "type": log.type,
+            "message": log.message
+        }
+
+        producer.handleLog(str(data))
+
+        return {"log_type": log.type, "log_message": log.message}
+    except Exception as e:
+        exceptionType, exceptionMessage = e.args
+        print("EXCEPTION TYPE: {}".format(exceptionType))
+        print("EXCEPTION MESSAGE: {}".format(exceptionMessage))
+
         return Response(
-            content='Invalid log type. Use either "info", "warning" or "error".',
+            content="Error processing the message",
             status_code=status.HTTP_400_BAD_REQUEST
         )
-    
-    data = {
-        "type": log.type,
-        "message": log.message
-    }
-
-    producer.handleLog(str(data))
-
-    return {"log_type": log.type, "log_message": log.message}
--- api/producer.py
+++ api/producer.py
@@ -4,19 +4,37 @@ class Producer:
     channel = None
     
     def connect(self):
-        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 
-        self.channel = connection.channel()
+            self.channel = connection.channel()
+        except pika.exceptions.AMQPConnectionError:
+            print("ERROR CONNECTION TO RABBITMQ")
     
     def handleLog(self, message: str):
-        if not self.channel:
+        try:
+            if self.channel is None or not self.channel.is_open:
+                self.connect()
+
+            self.channel.exchange_declare(exchange="logs", exchange_type="direct", durable=True)
+
+            print("sending message: {}".format(message))
+
+            self.channel.basic_publish(exchange='logs',
+                                        routing_key='',
+                                        body=message,
+                                        properties=pika.BasicProperties(
+                                            delivery_mode = pika.spec.PERSISTENT_DELIVERY_MODE
+                                            ))
+        except pika.exceptions.ConnectionClosed:
+            print("Connection closed. Trying to reconnect")
             self.connect()
-
-        self.channel.exchange_declare(exchange="logs", exchange_type="direct", durable=True)
-
-        self.channel.basic_publish(exchange='logs',
-                                    routing_key='',
-                                    body=message,
-                                    properties=pika.BasicProperties(
-                                        delivery_mode = pika.spec.PERSISTENT_DELIVERY_MODE
-                                        ))
+            self.handleLog(message)
+        except pika.exceptions.ConnectionClosedByBroker:
+            print(">>> Connection was closed by the broker")
+            raise Exception("BrokerError", "Could not deliver the message to the broker")
+        except pika.exceptions.ChannelWrongStateError:
+            print("ChannelError", "Failed to use the channel")
+            raise Exception("ChannelError", "Failed to use the channel")
+        except AttributeError:
+            raise Exception("ReconnectError", "Could not reconnect")
